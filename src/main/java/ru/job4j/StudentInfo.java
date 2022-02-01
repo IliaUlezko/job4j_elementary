12f9d13e6a0778ba7a0389506c4874febb5619aa
raiko <raikobob@yandex.ru>
2022-02-01 11:30:06 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Ilia Ulezko");
+        System.out.println("02.02.1989");
+
+    }
+}
