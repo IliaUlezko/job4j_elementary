3e4392bf9a6492251d1317441c00b1d1faa00748
raiko <raikobob@yandex.ru>
2022-02-20 12:00:12 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -42,4 +42,37 @@ public class FindLoopTest {
         int expected = 3;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenFind3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNotFindMinus1() {
+        int[] data = new int[] {99, 2, 98, 8, 7, 14, 111};
+        int el = 2;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenFind4() {
+        int[] data = new int[] {0, 12, 1, 2, 3, 4, 5, 6};
+        int el = 3;
+        int start = 1;
+        int finish = 6;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 4;
+        Assert.assertEquals(expected, result);
+    }
 }
