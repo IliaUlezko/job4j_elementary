0086c071341d45bd9153e5d67dec3f1369cb9f0c
raiko <raikobob@yandex.ru>
2022-03-04 11:14:50 +0300

    Перегрузить метод max для трех чисел

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,25 +3,14 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int left, int right) {
-       int rsl = left > right ? left : right;
-       return rsl;
+       return left >= right ? left : right;
     }
-}
 
-class MaxTest {
+    public static int max(int a, int b, int c) {
+        return max(a, max(b, c));
+    }
 
-    public static void main(String[] args) {
-        int a = 3;
-        int b = 4;
-        int c = Max.max(a, b);
-        System.out.println(c);
-        a = 5;
-        b = 2;
-        c = Max.max(a, b);
-        System.out.println(c);
-        a = 4;
-        b = 4;
-        c = Max.max(a, b);
-        System.out.println(c);
+    public static int max(int a, int b, int c, int d) {
+        return max(a, max(b, c, d));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -3,8 +3,6 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MaxTest {
 
     @Test
@@ -20,7 +18,7 @@ public class MaxTest {
     public void when3To4Then4() {
         int left = 3;
         int right = 4;
-        int result = Max.max(3, 4);
+        int result = Max.max(left, right);
         int expected = 4;
         Assert.assertEquals(result, expected);
     }
@@ -29,7 +27,7 @@ public class MaxTest {
     public void when7To5Then7() {
         int left = 7;
         int right = 5;
-        int result = Max.max(7, 5);
+        int result = Max.max(left, right);
         int expected = 7;
         Assert.assertEquals(result, expected);
     }
@@ -38,8 +36,39 @@ public class MaxTest {
     public void when3To3ThenEquals() {
         int left = 3;
         int right = 3;
-        int result = Max.max(3, 3);
+        int result = Max.max(left, right);
         int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void when3To5To7Then7() {
+        int a = 3;
+        int b = 5;
+        int c = 7;
+        int result = Max.max(a, b, c);
+        int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void when7To5To7Then7() {
+        int a = 7;
+        int b = 5;
+        int c = 7;
+        int result = Max.max(a, b, c);
+        int expected = 7;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void when3To5To7To11Then11() {
+        int a = 3;
+        int b = 5;
+        int c = 7;
+        int d = 11;
+        int result = Max.max(a, b, c, d);
+        int expected = 11;
         Assert.assertEquals(result, expected);
     }
 }
