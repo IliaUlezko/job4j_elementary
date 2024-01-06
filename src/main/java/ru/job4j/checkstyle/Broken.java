80825658f39084a34bfab9858552fb7af8db6566
IliaUlezko <raikobob@yandex.ru>
2024-01-06 12:00:44 +0300

    Правила Checkstyle

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String name;
+    public String surname;
+    public static final String NEW_VALUE = "";
+
+    public Broken() {
+
+    }
+
+    public void echo() {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c) {
+        System.out.print(a + b + c);
+    }
+}
